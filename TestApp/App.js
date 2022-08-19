438a1b47c3ab0ee6ae4b2981754b3ffc0de239b0
jingwei55 <e0725540@u.nus.edu>
2022-08-19 15:40:55 +0800

    testing 2

--- TestApp/App.js
+++ TestApp/App.js
@@ -1,17 +1,18 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 //core components, must import elements you are using
 
 export default function App() {
   //put all code into this root component
   return (
     <View style={styles.container}>
-      {/* View can only hold other components 
-       Can add more nested view components to add different styles */}
-      <Text>Hello World!</Text>
-      <Text style={styles.dummyText}>Sup bro</Text>
-      <Button title="Tap me!" />
-      {/* <StatusBar style="auto" /> */}
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals..</Text>
+      </View>
     </View>
   );
 }
@@ -19,15 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   //style sheet object to reuse styles, can add different styles in 1 style sheet
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 50,
   },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "blue",
-    padding: 16,
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%", //takes up percentage of the width of the container
+    marginRight: 8, //space between text-boxes
+    padding: 8, //for text to be not touching the edge
   },
 });
